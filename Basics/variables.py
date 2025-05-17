af6a48b36f758326d41395c8045f03ce0c177946
karpit <coronanegative@hotmail.com>
2025-05-17 07:50:27 -0700

    Added some programs

---
+++ Basics/variables.py
@@ -0,0 +1,13 @@
+# Variable = A container for a value (string , integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+
+#Strings
+first_name = "karpit"
+food = "J"
+email ="karpithere@gmail.com"
+
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
