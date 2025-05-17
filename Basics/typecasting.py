76e80897b86dcfbf343bcc7da92102c4cae96ee7
karpit <coronanegative@hotmail.com>
2025-05-17 08:03:48 -0700

    Added some more programs

--- Basics/typecasting.py
+++ Basics/typecasting.py
@@ -3,3 +3,13 @@
 
 
 
+name = "Arpit Kumar Singh"
+School = ""
+age = 27
+gpa = 3.5
+is_student = False
+
+name = bool(name)
+School = bool(School)
+
+print(f"{name} and {School}") # We can check whether the user has entered something or not
